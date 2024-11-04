17fbdab00ebf9346451959d08cbcf87310f3a34d
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-04 15:58:21 +0100

    4 fatto

---
+++ 04-impl-calculator/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public double add(double primo, double secondo) {
+      return primo+secondo;
+    }
+    public double sub(double primo, double secondo) {
+      return primo-secondo;
+    }
+    public double mul(double primo, double secondo) {
+      return primo*secondo;
+    }
+    public double div(double primo, double secondo) {
+      return primo/secondo;
+    }
+}
