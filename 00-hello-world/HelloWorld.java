c5cf8a39c2e2ba2737c9df66f4655b6727f67085
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-04 15:49:28 +0100

    1 fatto

--- 00-hello-world/HelloWorld.java
+++ 00-hello-world/HelloWorld.java
@@ -9,6 +9,6 @@ class HelloWorld {
         /*
          * Stampa "Hello World!" in standard output
          */
-        System.out.println("Hello, world!");
+        System.out.println("world! "+(50*50+50));
     }
 }
